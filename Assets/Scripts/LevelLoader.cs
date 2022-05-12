2bb824f6c43a181bb469191802658e98e6f7cf75
Van Damage <jeannoelseneque@hotmail.com>
2022-05-12 14:20:26 +0930

    Worked on Main Menu and scene transitions

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private Animator _animator;
+
+    [SerializeField] private float _transitionTime = 1f;
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        if (_animator == null)
+        {
+            Debug.LogError("Missing the animator for level transitions");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    LoadNextLevel();
+        //}
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+    }
+
+    IEnumerator LoadLevel(int leveIndex)
+    {
+        _animator.SetTrigger("Start");
+        yield return new WaitForSeconds(_transitionTime);
+        SceneManager.LoadScene(leveIndex);
+    }
+}
